acf9bde6524ee3f4e3358d45bf730be435674293
bcpratt2000@gmail.com <bcpratt2000@gmail.com>
2017-09-19 12:13:51 -0600

    optimised Code speed

--- src/org/usfirst/frc/team4585/robot/Launcher.java
+++ src/org/usfirst/frc/team4585/robot/Launcher.java
@@ -2,68 +2,56 @@ package org.usfirst.frc.team4585.robot;
 
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.Timer;
+
 public class Launcher {
-	
+
 	int fireValvePort;
 	int magazineSolenoidPort;
-	
+
 	boolean hasMagazineSolenoid;
-	
+
 	Solenoid fireValve;
 	Solenoid magazineSolenoid;
-	
+
 	public Launcher(int cannonTriggerPort) {
-		this.fireValvePort = cannonTriggerPort;	
-		
+		this.fireValvePort = cannonTriggerPort;
+
 		fireValve = new Solenoid(fireValvePort);
-		
+
 		hasMagazineSolenoid = false;
 	}
-	
-	public Launcher(int cannonTriggerPort, int  magazineSolenoidPort) {
-		this.fireValvePort = cannonTriggerPort;	
+
+	public Launcher(int cannonTriggerPort, int magazineSolenoidPort) {
+		this.fireValvePort = cannonTriggerPort;
 		this.magazineSolenoidPort = magazineSolenoidPort;
-		
+
 		fireValve = new Solenoid(fireValvePort);
 		magazineSolenoid = new Solenoid(magazineSolenoidPort);
-		
+
 		hasMagazineSolenoid = true;
 	}
-	
+
 	public void setFiring(boolean state) {
-		if(state) {
-			setAirLoad(false);
-			fireValve.set(true);
-		}
-		else {
-			fireValve.set(true);
-			setAirLoad(true);
-		}
+		setAirLoad(!state);
+		fireValve.set(state);
+
 	}
-	
+
 	public void fire() {
 		timedFire(15);
 	}
+
 	public void timedFire(double millis) {
-		
+
 		setAirLoad(false);
 		fireValve.set(true);
-		Timer.delay(millis/1000);
+		Timer.delay(millis / 1000);
 		fireValve.set(true);
 		setAirLoad(true);
 	}
-	
+
 	public void setAirLoad(boolean state) { // true is open, false is closed
-		if(hasMagazineSolenoid)
+		if (hasMagazineSolenoid)
 			magazineSolenoid.set(!state);
 	}
 }
-
-
-
-
-
-
-
-
-
--- src/org/usfirst/frc/team4585/robot/TankDrive.java
+++ src/org/usfirst/frc/team4585/robot/TankDrive.java
@@ -3,95 +3,76 @@ package org.usfirst.frc.team4585.robot;
 import edu.wpi.first.wpilibj.RobotDrive;
 
 public class TankDrive {
-	
+
 	private int motorLeft;
 	private int motorRight;
 	private RobotDrive chassis;
-	
+
 	private double coeffX = .75;
 	private double coeffY = .75;
 	private double scaleX = 2;
 	private double scaleY = 2;
 	private double deadX = .10;
 	private double deadY = .10;
-	
+
 	public TankDrive(int leftPort, int rightPort) {
 		motorLeft = leftPort;
 		motorRight = rightPort;
 		chassis = new RobotDrive(motorLeft, motorRight);
 		chassis.setExpiration(.1);
 	}
-	
+
 	public void arcadeDrive(double magX, double magY) { // x is rotation, y is forward/back
 		magX = applyDeadzone(magX, deadX);
 		magY = applyDeadzone(magY, deadY);
-		
-		magX = applyCoeff(magX, coeffX);
-		magY = applyCoeff(magY, coeffY);
-		
+
+		// magX = applyCoeff(magX, coeffX);
+		// magY = applyCoeff(magY, coeffY);
+
 		magX = applyScale(magX, scaleX);
 		magY = applyScale(magY, scaleY);
-		
+
 		chassis.arcadeDrive(magY, magX);
 	}
-	
+
 	private double applyDeadzone(double input, double deadzone) {
-		if(Math.abs(input) >= deadzone) {
+		if (Math.abs(input) >= deadzone) {
 			return input;
-		}
-		else {
+		} else {
 			return 0;
 		}
 	}
-	
+
 	private double applyCoeff(double input, double coeff) {
-		return input*coeff;
+		return input * coeff;
 	}
-	
+
 	private double applyScale(double input, double power) {
 		return Math.copySign(Math.pow(input, power), input);
-		
+
 	}
-	
-	
-	
+
 	public void setDeadzoneX(double deadzone) {
 		this.deadX = deadzone;
 	}
-	
+
 	public void setDeadzoneY(double deadzone) {
 		this.deadY = deadzone;
 	}
-	
+
 	public void setCoefficientX(double coeff) {
 		this.coeffX = coeff;
 	}
-	
+
 	public void setCoefficientY(double coeff) {
 		this.coeffY = coeff;
 	}
-	
+
 	public void setScaleX(double scale) {
 		this.scaleX = scale;
 	}
-	
+
 	public void setScaleY(double scale) {
 		this.scaleY = scale;
 	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
